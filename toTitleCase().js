5f6310371a0e56dc42d95341c966fee599d5f03e
Ofek Shmuely <32801785+ofekshmuely@users.noreply.github.com>
2022-12-09 13:43:07 +0200

    Update toTitleCase().js

--- toTitleCase().js
+++ toTitleCase().js
@@ -1,12 +1,25 @@
+/* toTitleCase
+Write a function that will capitalize every word in a sentence.  
 
+Example Input: "everything, everywhere, all at once"
+Example Output: "Everything, Everywhere, All At Once"
+*/
+
+/* 
+First, write a function that takes in one word and 
+capitalizes the first letter of that word.
+
+Example Input: "scrimba"
+Example Output: "Scrimba"
+
+Hint: Trying using slice() and .toUpperCase()
+*/
 
 function capitalizeWord(word){
     const firstUpperLetter = word.slice(0,1).toUpperCase()
     const afterUpper = word.slice(1)
     return firstUpperLetter+afterUpper
 }
-
-
 
 function toTitleCase(str){
     let myWords = str.split(' ');
@@ -18,6 +31,6 @@ function toTitleCase(str){
 }
 
 // Test your functions
-console.log(capitalizeWord("pumpkin"));
-console.log(toTitleCase("pumpkin pranced purposefully across the pond")); 
-console.log(toTitleCase("ganan gidel dagan bagan, dagan gadol gadal bagan"));
+console.log(capitalizeWord("pumpkin"));//Pumpkin
+console.log(toTitleCase("pumpkin pranced purposefully across the pond")); //Pumpkin Pranced Purposefully Across The Pond 
+console.log(toTitleCase("ganan gidel dagan bagan, dagan gadol gadal bagan")); //Ganan Gidel Dagan Bagan, Dagan Gadol Gadal Bagan
